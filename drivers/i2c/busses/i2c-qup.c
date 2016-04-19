01489189e6fac9a423e97709c65e307751422711
Andy Gross <andy.gross@linaro.org>
2016-04-19 16:41:53 -0500

    i2c: qup: Fix crash during suspend
    This patch corrects the suspend/resume handlers to correctly call the
    runtime pm calls when necessary.
    
    Signed-off-by: Andy Gross <andy.gross@linaro.org>

--- drivers/i2c/busses/i2c-qup.c
+++ drivers/i2c/busses/i2c-qup.c
@@ -1608,7 +1608,9 @@ static int qup_i2c_pm_resume_runtime(struct device *device)
 #ifdef CONFIG_PM_SLEEP
 static int qup_i2c_suspend(struct device *device)
 {
-	qup_i2c_pm_suspend_runtime(device);
+	if (!pm_runtime_suspended(device))
+		qup_i2c_pm_suspend_runtime(device);
+
 	return 0;
 }
 
